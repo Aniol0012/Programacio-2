fbb118eb8c09d808591be27228b2f61bfe84296f
Aniol <aniol0012@gmail.com>
2023-03-01 12:57:12 +0100

    Problem2_1 done

--- Activitats/src/Tema_2/src/Problem2_1.java
+++ Activitats/src/Tema_2/src/Problem2_1.java
@@ -1,6 +1,5 @@
 package src;
 
-import acm.graphics.GLabel;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
@@ -8,14 +7,38 @@ import java.awt.*;
 
 
 public class Problem2_1 extends GraphicsProgram {
-    public double TIMEOUT = 1000.0; // 1 sec
+    public double TIMEOUT = 5.0;
 
     public void run() {
-        GRect rectangle = new GRect(100, 75.0, 30.0, 50.0);
-        rectangle.setFillColor(Color.RED);
+        double rectWidth = 50.0;
+        double rectHeight = 50.0;
+        double inicialX = (getWidth() / 2.0) - (rectWidth / 2.0); // Both in the middle of the screen
+        double inicialY = (getHeight() / 2.0) - (rectHeight / 2.0);
+
+        GRect rectangle = new GRect(inicialX, inicialY, rectWidth, rectHeight);
         add(rectangle);
-
-
+        rectangle.setFilled(true);
+        rectangle.setFillColor(Color.RED);
+        double position = rectangle.getX(); // Actual position;
+        boolean shiftLeft = true;
+        while (true) {
+            if (position >= 0 && shiftLeft) { // Shifting to the left
+                if (position == 0) {
+                    shiftLeft = false;
+                    continue;
+                }
+                rectangle.move(-1, 0);
+                pause(TIMEOUT);
+            } else if (position <= getWidth() - rectWidth) { // Otherwise shifting to the right
+                if (position == getWidth() - rectWidth) {
+                    shiftLeft = true;
+                    continue;
+                }
+                rectangle.move(1, 0);
+                pause(TIMEOUT);
+            }
+            position = rectangle.getX(); // Gets the actual position
+        }
     }
 
     public static void main(String[] args) {
