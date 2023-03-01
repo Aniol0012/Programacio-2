aba5a9f97063d0cd0e1fa98a1e6c2cf0c4d2a122
Aniol <aniol0012@gmail.com>
2023-03-01 13:27:55 +0100

    Problem2_2 done

--- Activitats/src/Tema_2/src/Problem2_1.java
+++ Activitats/src/Tema_2/src/Problem2_1.java
@@ -45,21 +45,3 @@ public class Problem2_1 extends GraphicsProgram {
         new Problem2_1().start(args);
     }
 }
-
-/*
-Haced un programa usando las bibliotecas gráficas del paquete acm
-que haga lo siguiente:
-    • Escriba un rectángulo pintado de rojo en el centro de la pantalla
-    • Mueva dicho rectángulo de izquierda a derecha de manera que
-      cuando llegue a uno de los extremos de la pantalla, “rebote” y
-      empiece a moverse en el sentido contrario
-
-Podéis hacer varias versiones:
-    • Cambiando la posición del rectángulo con el método
-      setLocation
-    • Cambiando la posición del rectángulo con el método move
-
-Usad constantes para los valores del tamaño del cuadrado, la duración
-de la pausa que hacéis para que el movimiento sea suave, el número de
-píxeles que os movéis a cada paso, etc.
- */
---
+++ Activitats/src/Tema_2/src/Problem2_2.java
@@ -0,0 +1,42 @@
+package src;
+
+import acm.graphics.GOval;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+
+import java.awt.*;
+
+
+public class Problem2_2 extends GraphicsProgram {
+    public double TIMEOUT = 5.0;
+    public int NUM_TURTLES = 10;
+
+    public void run() {
+        GOval[] references = new GOval[NUM_TURTLES];
+        int radius = getHeight() / NUM_TURTLES;
+        for (int i = 0; i < NUM_TURTLES; i++) {
+            GOval turtle = new GOval(0, i * radius, radius, radius);
+            add(turtle);
+            turtle.setFilled(true);
+            turtle.setFillColor(Color.GREEN);
+            references[i] = turtle;
+        }
+
+        while (true) {
+            for (int i = 0; i < NUM_TURTLES; i++) {
+                references[i].move(rollDie(), 0);
+                pause(TIMEOUT);
+            }
+        }
+    }
+
+    public int rollDie() {
+        RandomGenerator rgen = RandomGenerator.getInstance();
+        return rgen.nextInt(1, 6);
+    }
+
+    public static void main(String[] args) {
+        new Problem2_2().start(args);
+    }
+}
